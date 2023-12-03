526096a148cfe89bf0005b52573bb423e130ad29
Sariafauzani <ajas2654@gmail.com>
2023-12-03 09:01:46 +0700

    modify: fitur input data

--- MAIN PROJECT HERE/FiturInputData.java
+++ MAIN PROJECT HERE/FiturInputData.java
@@ -4,14 +4,17 @@ public class FiturInputData {
     public static void main(String[] args) {
         String namaKaryawan, jabatan;
         String cetak;
+        int no.rek;
         //Menyimpan input dari user melalui scanner
         Scanner input = new Scanner(System.in);
     
         //Membaca input data karyawan
         System.out.println("Masukkan Nama Karyawan: ");
         namaKaryawan = input.nextLine();
-        System.out.println("Masukkan Jabatan: ");
+        System.out.println("Posisi: ");
         jabatan = input.nextLine();
+        System.out.println("No. Rekening: ")
+        no.rek = input.nextInt();
 
         //Menampilkan pilihan untuk mencetak data karyawan
         System.out.println("Apakah anda ingin mencetak data karyawan? Ya/Tidak");
@@ -19,10 +22,12 @@ public class FiturInputData {
 
         //Menampilkan data karyawan
         if (cetak.equalsIgnoreCase("Ya")) {
+            System.out.println("=================================");
             System.out.println("--------| Data Karyawan |--------");
-            System.out.println("---------------------------------");
+            System.out.println("================================="/n);
             System.out.println("Nama Karyawan: " + namaKaryawan);
             System.out.println("Jabatan: " + jabatan);
+            System.out.println("No. Rekening: " + no.rek);
         }else if (cetak.equalsIgnoreCase("Tidak")) {
         
         }
